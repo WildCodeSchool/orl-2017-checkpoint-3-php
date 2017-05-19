db578e0851da774f6e46c2bde3a596d77ea4f768
Quentin RIANDIERE <quentin.riandiere@gmail.com>
2017-05-19 09:39:07 +0200

    Algo

--- src/SubjectBundle/Controller/AlgoController.php
+++ src/SubjectBundle/Controller/AlgoController.php
@@ -12,7 +12,21 @@ class AlgoController extends Controller
     //
 
     public function dateInterval ($series) {
-
+        ksort($series);
+        $dates = array_flip($series);
+        $dateInterval = 0;
+        foreach ($dates as $date){
+            $nextDate = next($dates);
+            if($nextDate!=NULL) {
+                $difference = abs($date - $nextDate);
+            }else{
+                $difference = 0;
+            }
+            if($difference>$dateInterval){
+                $dateInterval = $difference;
+            }
+        }
+        return $dateInterval;
     }
 
 }

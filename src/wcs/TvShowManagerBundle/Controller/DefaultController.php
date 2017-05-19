b9ad07f87592fdffb45eb1047b2ebf4185ba0ba7
Necromosis <cabezasmaxime@gmail.com>
2017-05-19 11:54:12 +0200

    crud marche pas

--- src/wcs/TvShowManagerBundle/Controller/DefaultController.php
+++ src/wcs/TvShowManagerBundle/Controller/DefaultController.php
@@ -10,8 +10,38 @@ class DefaultController extends Controller
     /**
      * @Route("/tvshow")
      */
-    public function indexAction()
+    public function tvshowAction()
     {
-        return $this->render('TvShowManagerBundle:Default:index.html.twig');
+        $tvShowList = $this->getDoctrine()
+            ->getRepository('TvShowManagerBundle:TvShow')
+            ->findAll();
+        return $this->render('TvShowManagerBundle:tvshow:tvshow.html.twig', array('series' => $tvShowList));
+    }
+    /**
+     * @Route("/tvshow/add")
+     */
+    public function tvshowAddAction()
+    {
+        return $this->render('TvShowManagerBundle:tvshow:add.html.twig');
+    }
+    /**
+     * @Route("/tvshow/delete/{id}")
+     */
+    public function tvshowDeleteAction($id)
+    {
+        $tvShowList = $this->getDoctrine()
+            ->getRepository('TvShowManagerBundle:TvShow')
+            ->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($tvShowList);
+        $em->flush();
+        return $this->redirectToRoute('/tvshow');
+    }
+    /**
+     * @Route("/tvshow/edit/{id}")
+     */
+    public function tvshowEditAction()
+    {
+        return $this->render('TvShowManagerBundle:tvshow:edit.html.twig');
     }
 }

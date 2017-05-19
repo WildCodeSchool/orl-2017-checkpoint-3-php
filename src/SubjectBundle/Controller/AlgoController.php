46a3614aa97c2a47481a1656317f5e22f891120e
Necromosis <cabezasmaxime@gmail.com>
2017-05-19 10:00:31 +0200

    algo fini

--- src/SubjectBundle/Controller/AlgoController.php
+++ src/SubjectBundle/Controller/AlgoController.php
@@ -12,7 +12,19 @@ class AlgoController extends Controller
     //
 
     public function dateInterval ($series) {
+        $tempo = 0;
+        $diff = 0;
+        ksort($series);
+        $seriesKeys = array_keys($series);
+        //$seriesName = array_values($series);
 
+        for($i=0;$i<count($seriesKeys)-1;$i++) {
+            $tempo = $seriesKeys[$i+1] - $seriesKeys[$i];
+            if($tempo > $diff){
+                $diff = $tempo;
+            }
+        }
+        return $diff;
     }
-
 }
+
